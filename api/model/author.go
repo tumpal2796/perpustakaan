e41eb0daace2873bbe6f4981c7955d17940c37a9
tumpal2796 <tumpal2796@gmail.com>
2019-04-24 14:50:22 +0700

    Mc change all api (#5)
    * third commit
    
    * change API -> api

--- api/model/author.go
+++ api/model/author.go
@@ -8,10 +8,10 @@ import (
 )
 
 type Author struct {
-	AuthorID   int64         `db:"author_id"`
-	Name       string        `db:"name"`
-	CreateTime time.Time     `db:"create_time"`
-	CreateBy   int64         `db:"create_by"`
-	UpdateBy   pq.NullTime   `db:"update_time"`
-	UpdateTime sql.NullInt64 `db:"update_by"`
+	AuthorID   int64         `db:"author_id" json:"author_id"`
+	Name       string        `db:"name" json:"name"`
+	CreateTime time.Time     `db:"create_time" json:"create_time"`
+	CreateBy   int64         `db:"create_by" json:"create_by"`
+	UpdateBy   pq.NullTime   `db:"update_time" json:"update_time"`
+	UpdateTime sql.NullInt64 `db:"update_by" json:"update_by"`
 }
--- api/model/publisher.go
+++ api/model/publisher.go
@@ -8,10 +8,10 @@ import (
 )
 
 type Publisher struct {
-	PublisherID int64         `db:"publisher_id"`
-	Name        string        `db:"name"`
-	CreateTime  time.Time     `db:"create_time"`
-	CreateBy    int64         `db:"create_by"`
-	UpdateBy    pq.NullTime   `db:"update_time"`
-	UpdateTime  sql.NullInt64 `db:"update_by"`
+	PublisherID int64         `db:"publisher_id" json:"publisher_id"`
+	Name        string        `db:"name" json:"name"`
+	CreateTime  time.Time     `db:"create_time" json:"create_time"`
+	CreateBy    int64         `db:"create_by" json:"create_by"`
+	UpdateBy    pq.NullTime   `db:"update_time" json:"update_time"`
+	UpdateTime  sql.NullInt64 `db:"update_by" json:"update_by"`
 }
